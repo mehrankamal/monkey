f6788bec8d19ff93bb54a4d506363eefa25960df
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-09-23 12:48:09 +0500

    Implement Lexing on a bigger Token set of Monkey language.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -23,6 +23,14 @@ func TestNextToken(t *testing.T) {
 		{token.EOF, ""},
 	}
 
+	assertNextTokens(t, input, testCases)
+
+}
+
+func assertNextTokens(t *testing.T, input string, testCases []struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}) {
 	lexer := New(input)
 
 	for i, testToken := range testCases {
@@ -38,5 +46,58 @@ func TestNextToken(t *testing.T) {
 				i, testToken.expectedLiteral, lexedToken.Literal)
 		}
 	}
+}
 
+func TestNextTokenExtendedSet(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+		x + y;
+	};
+	let result = add(five, ten);
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	assertNextTokens(t, input, tests)
 }
