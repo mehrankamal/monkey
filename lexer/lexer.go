2ef062379eed7cf713102a5b6c8c4b9db231ec58
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-09-23 13:50:18 +0500

    Extend token set to include other single char operators

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -36,6 +36,10 @@ func (l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -44,8 +48,16 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.RPAREN, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
+	case '!':
+		tok = newToken(token.BANG, l.ch)
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
