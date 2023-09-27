d983040c57fb8964dcf0e32d71985794d69cef23
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-09-28 00:23:42 +0500

    Parse grouped expressions.

--- parser/parser.go
+++ parser/parser.go
@@ -76,6 +76,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefixFunc(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefixFunc(token.TRUE, p.parseBoolean)
 	p.registerPrefixFunc(token.FALSE, p.parseBoolean)
+	p.registerPrefixFunc(token.LPAREN, p.parseGroupedExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfixFunc(token.PLUS, p.parseInfixExpression)
@@ -280,4 +281,16 @@ func (p *Parser) parseBoolean() ast.Expression {
 		Token: p.currentToken,
 		Value: p.currentTokenIs(token.TRUE),
 	}
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
