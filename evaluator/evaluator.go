fb2be949597647b439ef2ef6c9621e710c42e1b6
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-09-29 04:21:09 +0500

    Use single true and false objects.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/mehrankamal/monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -16,10 +21,18 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
+}
+
+func nativeBoolToBooleanObject(value bool) object.Object {
+	if value {
+		return TRUE
+	}
+
+	return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
