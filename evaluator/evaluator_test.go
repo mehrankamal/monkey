2fcdde66bcb7d013ffd80b8ddd5564016c5bed96
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-09-29 04:18:17 +0500

    Evaluate booleans.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -21,11 +21,41 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestBooleanExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tc := range tests {
+		evaluated := evalInput(tc.input)
+		assertBooleanObject(t, evaluated, tc.expected)
+	}
+}
+
 func evalInput(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
 	return Eval(program)
+}
+
+func assertBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object not Boolean, got %T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("Object has wrong value got=%t, want=%t", result.Value, expected)
+		return false
+	}
+
+	return true
 }
 
 func assertIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
