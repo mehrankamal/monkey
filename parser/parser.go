ebf83f99170bb09df07ea44be72b3834c2b10acf
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-10-02 18:28:01 +0500

    Parse string tokens into AST structure.

--- parser/parser.go
+++ parser/parser.go
@@ -80,6 +80,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefixFunc(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefixFunc(token.IF, p.parseIfExpression)
 	p.registerPrefixFunc(token.FUNCTION, p.parseFunctionLiteral)
+	p.registerPrefixFunc(token.STRING, p.parseStringLiteral)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfixFunc(token.PLUS, p.parseInfixExpression)
@@ -429,4 +430,8 @@ func (p *Parser) parseCallArguments() []ast.Expression {
 	}
 
 	return args
+}
+
+func (p *Parser) parseStringLiteral() ast.Expression {
+	return &ast.StringLiteral{Token: p.currentToken, Value: p.currentToken.Literal}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -671,6 +671,26 @@ func assertLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	return true
 }
 
+func TestStringLiteralExpression(t *testing.T) {
+	input := `"hello world";`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	assertNoParserErrors(t, p)
+
+	stmt := program.Statements[0].(*ast.ExpressionStatement)
+
+	literal, ok := stmt.Expression.(*ast.StringLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.StringLiteral. got=%T", stmt.Expression)
+	}
+
+	if literal.Value != "hello world" {
+		t.Errorf("literal.Value not %q. got=%q", "hello world", literal.Value)
+	}
+}
+
 func assertNoParserErrors(t *testing.T, parser *Parser) {
 	errors := parser.Errors()
 
