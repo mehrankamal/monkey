fcee8969fcc4dd2e2d53a09a3e0b7182a54c72aa
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-10-06 16:48:00 +0500

    Implement OpConstant instruction generation for integer literals.

---
+++ compiler/compiler.go
@@ -0,0 +1,85 @@
+package compiler
+
+import (
+	"github.com/mehrankamal/monkey/ast"
+	"github.com/mehrankamal/monkey/code"
+	"github.com/mehrankamal/monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		value := &object.Integer{Value: node.Value}
+		address := c.addConstant(value)
+		c.emit(code.OpConstant, address)
+	}
+
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(opcode code.Opcode, operands ...int) int {
+	instruction := code.Make(opcode, operands...)
+	pos := c.addInstruction(instruction)
+	return pos
+}
+
+func (c *Compiler) addInstruction(instruction []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, instruction...)
+
+	return posNewInstruction
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
