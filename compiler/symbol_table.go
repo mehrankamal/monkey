49a67f6b2d59271c9117ce099cd3f824fedb18a8
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-10-09 15:42:31 +0500

    Set up symbol table and Define variable operation.

---
+++ compiler/symbol_table.go
@@ -0,0 +1,32 @@
+package compiler
+
+type SymbolScope string
+
+const (
+	GlobalScope SymbolScope = "GLOBAL"
+)
+
+type Symbol struct {
+	Name  string
+	Scope SymbolScope
+	Index int
+}
+
+type SymbolTable struct {
+	store          map[string]Symbol
+	numDefinitions int
+}
+
+func (st *SymbolTable) Define(name string) Symbol {
+	symbol := Symbol{Name: name, Index: st.numDefinitions, Scope: GlobalScope}
+
+	st.store[name] = symbol
+	st.numDefinitions += 1
+
+	return symbol
+}
+
+func NewSymbolTable() *SymbolTable {
+	s := make(map[string]Symbol)
+	return &SymbolTable{store: s}
+}
