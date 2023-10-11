0ecea2abf23d8edb656d1fa32773d006000a0a6d
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-10-11 15:56:15 +0500

    Implement local compilation scopes in compiler.

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -80,7 +80,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 		symbol := c.symbolTable.Define(node.Name.Value)
-		c.emit(code.OpSetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpSetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpSetLocal, symbol.Index)
+		}
 
 	case *ast.PrefixExpression:
 		err := c.Compile(node.Right)
@@ -194,7 +198,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if !ok {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
-		c.emit(code.OpGetGlobal, symbol.Index)
+
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpGetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpGetLocal, symbol.Index)
+		}
 
 	case *ast.ArrayLiteral:
 		for _, elem := range node.Elements {
@@ -357,6 +366,8 @@ func (c *Compiler) enterScope() {
 	}
 	c.scopes = append(c.scopes, scope)
 	c.scopeIndex++
+
+	c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 }
 
 func (c *Compiler) leaveScope() code.Instructions {
@@ -364,6 +375,8 @@ func (c *Compiler) leaveScope() code.Instructions {
 
 	c.scopes = c.scopes[:len(c.scopes)-1]
 	c.scopeIndex--
+
+	c.symbolTable = c.symbolTable.Outer
 
 	return instructions
 }
